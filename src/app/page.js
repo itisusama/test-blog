026ae5450225f4d615a2b2c197af0780bf212cd6
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-07-30 17:40:35 +0500

    test

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+// page.js
+
+import React from 'react'
+import BlogList from '@/Components/BlogList';
+
+const page = () => {
+    return (
+        <>
+            <BlogList />
+        </>
+    )
+}
+
+export default page
